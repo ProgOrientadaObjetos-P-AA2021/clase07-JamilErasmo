653874175c7353ab143cf63f71586bb273d48866
JamilErasmo <jamilerasmoq@gmail.com>
2021-05-19 18:43:38 -0500

    taller06

---
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete6;
+
+public class Hospital {
+    String nombre;
+    int numeroCamas;
+    double presupuesto;
+
+    Hospital(String caritas, int i, double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroCamas() {
+        return numeroCamas;
+    }
+
+    public void setNumeroCamas(int numeroCamas) {
+        this.numeroCamas = numeroCamas;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+    
+}
+
+/* 
+Agregar valor de objetos de tipo Hospital a un archivo
+Y leer el archivo
+*/
